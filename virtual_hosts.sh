badff4626904d1338a43b84cfac11daf739524ea
Francisco Anguiano <fespinosa@devsalsa.com>
2021-05-24 10:05:01 -0500

    added remove virtual host

--- virtual_hosts.sh
+++ virtual_hosts.sh
@@ -3,15 +3,28 @@
 # Run as SU
 # @TODO enable https (cerbot)
 
+SCRIPT=$( basename "$0" )
+version='1.0.0'
+delete=false
 
-#catch DNS
+while getopts "d" opt; do
+  case $opt in 
+    d) delete=true
+    ;;
+  esac
+done
 
+#Set vars
 DNS=$1
 HTTPS=$2
 
+if [ "$delete" = true ]; then  
+  DNS=$2
+  HTTPS=$3
+fi
 
 #Validate
-if [ -z "$1" ]
+if [ -z "$DNS" ]
   then
     echo "usage: ./virtual_host.sh example.com"
     exit 1
@@ -21,6 +34,17 @@ fi
 DocumentRoot=/var/www/html/mds-tools/www/en/funnels/pages/
 vhFile="000-$DNS.conf"
 pathfileVH="/etc/apache2/sites-available/$vhFile"
+
+
+if [ "$delete" = true ]; then 
+  echo "delete $DNS"
+  cd /etc/apache2/sites-available/
+  a2dissite $vhFile
+  rm $vhFile
+  service apache2 restart
+  exit 0
+fi
+
 
 
 #create file
